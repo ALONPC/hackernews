9758271d52d2929d6ac999d71ba698c811e43c28
ALONPC <alon.berrios@gmail.com>
2020-07-29 17:50:56 -0400

    cron functionality

--- server/server.js
+++ server/server.js
@@ -4,16 +4,13 @@ const app = express();
 const Hit = require("./models/Hit");
 const cors = require("cors");
 const fetch = require("node-fetch");
+const cron = require("node-cron");
 app.use(cors());
 require("dotenv").config();
 require("./routes/index")(app);
 
 const PORT = process.env.PORT || 8001;
 const DBHOST = process.env.DBHOST || "localhost";
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}, host ${DBHOST}`);
-});
 
 const connectDb = async () => {
   try {
@@ -30,27 +27,30 @@ const connectDb = async () => {
 connectDb();
 
 const seedDb = async () => {
-  const existingHits = await Hit.find().exec();
-  if (!!existingHits.length) {
-    console.log("Up to date!");
-  } else {
-    const {
-      hits,
-    } = await fetch(
-      "https://hn.algolia.com/api/v1/search_by_date?query=nodejs",
-      { method: "GET" }
-    ).then((res) => res.json());
-    for (const hit of hits) {
-      const newHit = new Hit(hit);
-      await newHit.save((err, obj) => {
-        if (err) {
-          return console.error(err);
-        }
-        console.log(`${obj.title || obj.story_title} saved in the collection`);
-      });
+  Hit.collection.drop();
+  const { hits } = await fetch(
+    "https://hn.algolia.com/api/v1/search_by_date?query=nodejs",
+    {
+      method: "GET",
     }
-    console.log("Seed completed!");
+  ).then((res) => res.json());
+  for (const hit of hits) {
+    const newHit = new Hit(hit);
+    await newHit.save((err, obj) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(`${obj.title || obj.story_title} saved in the collection`);
+    });
   }
+  console.log("Seed completed!");
 };
 
-seedDb();
+// runs the seedDB every hour
+cron.schedule("0 0 */1 * * *", () => {
+  seedDb();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}, host ${DBHOST}`);
+});
